3ba690326e80fe0ba6b9c408e3264e490d06e796
CrankyTitanO7 <nedajtheawesome@gmail.com>
2026-01-23 02:07:42 -0500

    almost main

--- cpsc2230/pset1/main.c
+++ cpsc2230/pset1/main.c
@@ -1,7 +1,7 @@
 /*
 Driver for Game of Life assignment in CPSC 223 Fall 2023
 
-Author: [your name here]
+Author: Jaden Lee
 */
 
 #include <stdlib.h>
@@ -24,19 +24,40 @@ int main(int argc, char *argv[])
      *      representing the initial state for this run of Game of Life.
      */
 
+     int time_steps = argv[0];
+     char init[] = argv[1];
+
     /*
      * TODO: Create a new statically-allocated array of size ROWS x COLS
      */
+
+     int arr[ROWS][COLS];
+     
+     int counter = 0;
+     for (int i = 0; i < ROWS; i ++) {
+        for (int j = 0; j < COLS; j ++){
+            arr[i][j] = init[counter];
+            counter++;
+        }
+     }
 
     /*
      * TODO: Print the initial values held in the field
      */
 
+     print_initial(arr);
     /*
      * TODO: Run the game time_steps times, printing the field at the end of
      * each step
      */
-
+     int i = 1;
+     int hold[ROWS][COLS] = arr;
+     while (i <= time_steps){
+        i++; 
+        compute_next_gen(hold, arr);
+        print_generation(arr, i);
+        hold = arr;
+    }
     /*
      * Print "Done." then exit.
      */
