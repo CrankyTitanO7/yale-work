81c6b9623fdcb07f4c4661540e96a23fc4ad189c
CrankyTitanO7 <nedajtheawesome@gmail.com>
2026-02-06 01:38:24 -0500

    lab complete

--- cpsc2230/lab2/hookbook_starter/PirateList.c
+++ cpsc2230/lab2/hookbook_starter/PirateList.c
@@ -4,40 +4,46 @@
 
 // Initialize the list
 void piratelist_init(List *list) {
-    list->length = MAX_PIRATES;
+    list->length = 0;
     // list->pirates[MAX_PIRATES];
 }
 
 
 // Read all lines from file and add to list. 
 int piratelist_add(List *list, FILE *file) {
+
+if (list->length >= MAX_PIRATES) {
+        return 0; 
+    }
+
     char line[LINE_MAX];
     // Read each line from the file with fgets preferably
     // create a Pirate struct for each line and copy it
     // add it to the list if there is space, use return 0 if full
     Pirate pir; 
 
-    FILE *names = fopen(* (file), "r");
+    // FILE *names = fopen(file, "r");
 
     int i = 0;
+    
 
-    while (line[i] != EOF && MAX_PIRATES - list->length < 1){
-        fgets(line, LINE_MAX, pir.name);
-        pir.pirate_id=i;
+while (list->length < MAX_PIRATES && fgets(line, LINE_MAX, file) != NULL) {
+        pir.pirate_id=i+1;
+        strcpy (pir.name, line);
         list->pirates[list->length] = pir;
         list->length ++;
         i++;
-
-        
     }
 
-    fclose(names);
+    // fclose(file);
 
     if (MAX_PIRATES - list->length < 1) {
         return 0;
     } else {
         return 1;
     }
+
+    // return 1;
     
 
 }
@@ -47,6 +53,10 @@ void piratelist_print(const List *list) {
     for (int i = 0; i < list->length; i++) {
         // print each pirate in the list
         // use the pirate_id and name fields
+
+        printf("%i", list->pirates[i].pirate_id);
+        printf(" ; ");
+        printf(list->pirates[i].name);
     }
     printf("\n");
 }
