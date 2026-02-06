88bc3cfe6e102fc04c9d30d77bca422c41f685ef
jaden <jaden.lee@yale.edu>
2026-02-06 00:55:28 -0500

    lab2?

--- cpsc2230/lab2/hookbook_starter/PirateList.c
+++ cpsc2230/lab2/hookbook_starter/PirateList.c
@@ -4,7 +4,8 @@
 
 // Initialize the list
 void piratelist_init(List *list) {
-    
+    list->length = MAX_PIRATES;
+    // list->pirates[MAX_PIRATES];
 }
 
 
@@ -14,6 +15,29 @@ int piratelist_add(List *list, FILE *file) {
     // Read each line from the file with fgets preferably
     // create a Pirate struct for each line and copy it
     // add it to the list if there is space, use return 0 if full
+    Pirate pir; 
+
+    FILE *names = fopen(* (file), "r");
+
+    int i = 0;
+
+    while (line[i] != EOF && MAX_PIRATES - list->length < 1){
+        fgets(line, LINE_MAX, pir.name);
+        pir.pirate_id=i;
+        list->pirates[list->length] = pir;
+        list->length ++;
+        i++;
+
+        
+    }
+
+    fclose(names);
+
+    if (MAX_PIRATES - list->length < 1) {
+        return 0;
+    } else {
+        return 1;
+    }
     
 
 }
